1ee2a6d8af2b96eac5e33c77a50ef5e79384d252
xiongmao <xiongmao666666>
2025-09-28 11:29:54 +0800

    bug修改

--- src/api/index.js
+++ src/api/index.js
@@ -8,7 +8,7 @@ import { useCommonStore } from "@/store/modules/common";
 import { errorMessages } from "./errorCodeMap";
 import i18n from "../i18n/index.js"; // 引入全局 i18n 实例
 // 获取 BASEPATH，确保 window.BASEPATH 存在
-const baseURL = window.VITE_API_BASE_URL ? window.VITE_API_BASE_URL :import.meta.env.VITE_API_BASE_URL; // 兜底默认值
+const baseURL = window.g.VITE_API_BASE_URL ? window.g.VITE_API_BASE_URL :import.meta.env.VITE_API_BASE_URL; // 兜底默认值
 
 const api = axios.create({
   baseURL: baseURL,
