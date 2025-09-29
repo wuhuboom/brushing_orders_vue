cb6f73b597eaf77920a8c59a8eeb387699091721
xiongmao <xiongmao666666>
2025-09-29 19:52:11 +0800

    修改打包时间搓

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path';
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import ElementPlus from 'unplugin-element-plus/vite'
-import NutUIResolver from '@nutui/auto-import-resolver'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import ElementPlus from "unplugin-element-plus/vite";
+import NutUIResolver from "@nutui/auto-import-resolver";
+const timestamp = new Date().getTime(); // 当前时间戳
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,22 +15,32 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver(),NutUIResolver()],
+      resolvers: [ElementPlusResolver(), NutUIResolver()],
     }),
     ElementPlus({
       // 对unplugin-element-plus的配置
       // 指定需要导入的样式，可以是'css'或'sass'
-      useSource: true
+      useSource: true,
     }),
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'), // 使用 resolve 来设置 '@' 别名指向 src 目录
+      "@": resolve(__dirname, "src"), // 使用 resolve 来设置 '@' 别名指向 src 目录
     },
   },
   server: {
     watch: {
-      usePolling: true,   // 修复HMR热更新失效
+      usePolling: true, // 修复HMR热更新失效
     },
-}
-})
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // 给 js、css、assets 加时间戳
+        entryFileNames: `assets/js/[name].[hash].${timestamp}.js`,
+        chunkFileNames: `assets/js/[name].[hash].${timestamp}.js`,
+        assetFileNames: `assets/[ext]/[name].[hash].${timestamp}.[ext]`
+      }
+    }
+  }
+});
