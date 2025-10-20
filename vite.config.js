59585da347d5c89fc18a66fc5072c62d1b4ee169
wuhuboom <119092971+wuhuboom@users.noreply.github.com>
2025-10-20 14:57:55 +0800

    打包前处理配置文件
    (cherry picked from commit 16c9598515a9512bf4d416d26d06a123c079e430)

--- vite.config.js
+++ vite.config.js
@@ -7,34 +7,35 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import ElementPlus from "unplugin-element-plus/vite";
 import NutUIResolver from "@nutui/auto-import-resolver";
 const timestamp = new Date().getTime(); // 当前时间戳
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import fs from "fs";
 import path from "path";
 
-function removeConfigJs() {
-  let resolvedOutDir = "dist";
-  return {
-    name: "remove-config-js",
-    configResolved(cfg) {
-      resolvedOutDir = cfg.build?.outDir || resolvedOutDir;
-    },
-    closeBundle() {
-      const target = path.resolve(resolvedOutDir, "config.js"); 
-      try {
-        if (fs.existsSync(target)) {
-          fs.unlinkSync(target);
-          console.log("[remove-config-js] 移除", target);
-        } else {
-          console.log("[remove-config-js] 跳过/没有发现:", target);
-        }
-      } catch (e) {
-        console.warn("[remove-config-js] 错误:", e);
-      }
-    },
-  };
-}
+// function removeConfigJs() {
+//   let resolvedOutDir = "dist";
+//   return {
+//     name: "remove-config-js",
+//     configResolved(cfg) {
+//       resolvedOutDir = cfg.build?.outDir || resolvedOutDir;
+//     },
+//     closeBundle() {
+//       const target = path.resolve(resolvedOutDir, "config.js"); 
+//       try {
+//         if (fs.existsSync(target)) {
+//           fs.unlinkSync(target);
+//           console.log("[remove-config-js] 移除", target);
+//         } else {
+//           console.log("[remove-config-js] 跳过/没有发现:", target);
+//         }
+//       } catch (e) {
+//         console.warn("[remove-config-js] 错误:", e);
+//       }
+//     },
+//   };
+// }
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     vue(),
     AutoImport({
@@ -48,7 +49,18 @@ export default defineConfig({
       // 指定需要导入的样式，可以是'css'或'sass'
       useSource: true,
     }),
-    removeConfigJs(),
+    // removeConfigJs(),
+
+    ...(command === "build"
+      ? [
+          viteStaticCopy({
+            targets: [
+              { src: ["public/*", "!public/config.js"], dest: "." },
+              { src: ["public/**/*", "!public/config.js"], dest: "." },
+            ],
+          }),
+        ]
+      : []),
   ],
   resolve: {
     alias: {
@@ -61,6 +73,7 @@ export default defineConfig({
     },
   },
   build: {
+    copyPublicDir: false,
     rollupOptions: {
       output: {
         // 给 js、css、assets 加时间戳
@@ -70,4 +83,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
