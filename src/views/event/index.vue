077c4d9e013c933aaedc41855df559c243a53e7c
xiongmao <xiongmao666666>
2026-01-17 22:27:35 +0800

    bug修改

--- src/views/event/index.vue
+++ src/views/event/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full min-h-[100vh] bg-[#f4f4f5]">
+  <div class="w-full min-h-[100vh] bg-[#f7f7f7]">
      <van-nav-bar
         :title="$t('事件')"
         fixed
@@ -7,10 +7,54 @@
         class="shadow"
         @click-left="onClickLeft"
     />
-    <div class="mt-[45px] w-full text-center flex flex-col items-center justify-center text-[24px] text-[#2D1810] vipsBg font-bold" >
-        <p class="pb-[20px]">{{$t('vips.str1')}} </p>
-        <p>{{$t('vips.str2')}}</p>
+    <div class="mt-[45px] w-full text-center flex flex-col items-center justify-center  vipsBg" >
+        <p class="pb-[8px] text-[20px] text-[#2D1810] font-bold">{{$t('event.title1')}} </p>
+        <p class="text-[#2D1810] text-[12px]">{{$t('event.title2')}}</p>
+    </div>
+    <div class="px-[16px] mt-[16px]">
+      <div class="grid grid-cols-4 gap-4 text-center place-items-center bg-[#2D1810] text-[11px] text-[#fff] py-[5px] px-[10px]">
+        <div>{{$t('event.title3')}}</div>
+        <div>{{$t('event.title4')}}</div>
+        <div>{{$t('event.title5')}}</div>
+        <div>{{$t('event.title6')}}</div>
       </div>
+      <div class="bg-[#fff] px-[10px]">
+        <div v-for="item in vipsArr" class="grid grid-cols-4 gap-4 text-center place-items-center text-[11px] text-[#000] py-[5px] px-[10px]" style="border-bottom: 1px solid #D0D0D0;">
+          <div>
+            <img class="w-[15px] h-[15px]" src="@/static/images/star.png" alt="">
+            VIP{{item.str1}}
+          </div>
+          <div>{{item.str2}}%</div>
+          <div><span>{{item.str3}}</span> {{$t('event.str')}}</div>
+          <div>USD{{item.str4}}</div>
+        </div>
+      </div>
+      <div class="text-[14px] text-[#2D1810] pt-[20px]">
+        {{$t('event.content1')}}
+      </div>
+      <div class="text-[12px] text-[#374151] pt-[14px]">
+        {{$t('event.content2')}}
+      </div>
+      <div class="text-[14px] text-[#2D1810] pt-[16px]">
+        {{$t('event.content3')}}
+      </div>
+      <div class="text-[12px] text-[#374151] pt-[14px]">
+        {{$t('event.content4')}}
+      </div>
+      <div class="text-[12px] text-[#374151] ">
+        {{$t('event.content5')}}
+      </div>
+      <div class="text-[12px] text-[#374151] ">
+        {{$t('event.content6')}}
+      </div>
+      <div class="text-[12px] text-[#374151]">
+        {{$t('event.content7')}}
+      </div>
+      <div class="text-[14px] text-[#2D1810] pt-[16px]">
+        {{$t('event.content8')}}
+      </div>
+      
+    </div>
   </div>
 </template>
 <script setup>
@@ -19,6 +63,8 @@ import { useCommonStore } from '@/store/modules/common';
 import {getConfigByLang} from "../../api/apis"
 const latestEventEn = ref('')
 const commonStore = useCommonStore();
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
 
 const parLang = computed(() => {
   const mapped = commonStore.getValueByKey(commonStore.lang);
@@ -31,6 +77,38 @@ const getGetGlobalConfig = async() =>{
     latestEventEn.value = res?.data?.latestEvent ?? '';
     console.log(latestEventEn.value)
 }
+const vipsArr = [
+  {
+    str1:1,
+    str2:0.5,
+    str3:40,
+    str4:'100-499'
+  },
+  {
+    str1:2,
+    str2:1.0,
+    str3:45,
+    str4:'500-1599'
+  },
+  {
+    str1:3,
+    str2:1.5,
+    str3:50,
+    str4:'1600-5499'
+  },
+  {
+    str1:4,
+    str2:2.0,
+    str3:55,
+    str4:'5500-9999'
+  },
+  {
+    str1:5,
+    str2:2.5,
+    str3:60,
+    str4:`10000-${t('event.str1')}`
+  }
+]
 onMounted(() =>{
     getGetGlobalConfig();
 })
@@ -39,8 +117,8 @@ const onClickLeft = () => history.back();
 </script>
 <style scoped>
   .vipsBg {
-   background: url(../../static/images/vipBg.png) no-repeat center;
+   background: url(../../static/images/eventBg.png) no-repeat center;
   background-size: cover;
-  height: 130px;
+  height: 80px;
 }
 </style>
--- src/views/guide/index.vue
+++ src/views/guide/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-full min-h-[100vh] bg-[#fff]">
+  <div class="w-full min-h-[100vh] bg-[#f7f7f7]">
     
-    <div class="w-full p-6 box-border flex flex-col font-montserrat text-[#666]">
+    <div class="w-full p-6 box-border flex flex-col">
         <van-nav-bar
         :title="$t('收入指南')"
         fixed
@@ -9,14 +9,83 @@
         class="shadow"
         @click-left="onClickLeft"
     />
-     <div v-html="incomeGuideEn" class="mt-[65px]"></div>
+    <div class="mt-[40px] text-[#000000]">
+      <div class="text-[12px] text-[#000] pb-[10px]" style="border-bottom: 0.5px solid #E5E7EB;">{{$t('guide.content1')}}</div>
+      <div class="text-[14px] mt-[18px]">{{$t('guide.title1')}}</div>
+      <div class="text-[12px] text-[#000] py-[10px]">{{$t('guide.content2')}}</div>
     </div>
+    <div class="bg-[#fff] rounded-[10px]" style="overflow: hidden;">
+      <div class="text-center bg-[#2D1810] text-[#fff] py-[7px]">
+        <div>{{$t('guide.title2')}}</div>
+        <div>{{$t('guide.title3')}}</div>
+      </div>
+      <div v-for="item in guide" class="grid grid-cols-3 gap-3 px-[10px] text-[#000] pt-[15px] pb-[8px]" style="border-bottom: 1px solid #D0D0D0;">
+        <div>{{$t('guide.title4')}}</div>
+        <div>{{item.day}}{{$t('guide.title5')}}</div>
+        <div class="text-right">USD{{item.money}}</div>
+      </div>
+      <div class="text-[#000] px-[5px] text-[12px] font-bold">{{$t('guide.title6')}}</div>
+      <div class="text-[#4B5563] px-[5px] text-[10px] leading-4 py-[5px]">{{$t('guide.content3')}}</div>
+      <div class="text-[#4B5563] px-[5px] text-[10px] leading-4 py-[5px]">{{$t('guide.content4')}}</div>
+      <div class="text-[#FF7D01] px-[5px] text-[10px] leading-4 py-[5px] font-bold">{{$t('guide.content5')}}</div>
+    </div>
+    <div class="mt-[40px] text-[#000000]">
+      <div class="text-[12px] text-[#000] pb-[10px]" style="border-bottom: 0.5px solid #E5E7EB;">{{$t('guide.content1')}}</div>
+      <div class="text-[14px] mt-[18px]">{{$t('guide.title7')}}</div>
+      <div class="text-[12px] text-[#000] py-[10px]">{{$t('guide.content6')}}</div>
+    </div>
+    <div class="guideBg">
+      <div class="text-center bg-[#2D1810] text-[#fff] py-[7px]">
+        <div>{{$t('guide.title2')}}</div>
+        <div>{{$t('guide.title3')}}</div>
+      </div>
+      <div class="grid grid-cols-3 gap-3 px-[10px] py-[12px]">
+        <div  v-for="item in guideArr">
+          <div class="bg-[#fff] text-center">
+            <div class="bg-[#2D1810] text-[#fff] text-[12px] font-bold">{{item.title}}</div>
+            <div class="text-[12px] text-[#2D1810] py-[3px]">{{$t('guide.title16')}}</div>
+            <div class="text-[#FF7D01] text-[9px] pb-[3px]">USD <span class="text-[14px] font-bold">{{ item.num }}</span></div>
+            <div class="text-[12px] text-[#2D1810]">{{$t('guide.title17')}}</div>
+            <div class="text-[#FF7D01] text-[9px] py-[3px]">USD <span class="text-[14px] font-bold">{{ item.num1 }}</span></div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[#FF7D01] text-[14px] font-bold text-center text-[#fff] px-[9px] py-[12px]">
+        ={{$t('guide.title18')}}
+      </div>
+      <div class="grid grid-cols-2 gap-2 px-[10px] py-[12px]">
+        <div  v-for="item in advanArr">
+          <div class="bg-[#fff] text-center">
+            <!-- <div class="bg-[#2D1810] text-[#fff] text-[12px] font-bold">{{item.title}}</div> -->
+            <div class="text-[12px] text-[#2D1810] py-[3px] font-bold">{{$t('guide.title19')}}(USD)</div>
+            <div class="text-[#FF7D01] text-[9px] pb-[3px]"><span class="text-[14px] font-bold">{{ item.num }}</span></div>
+            <div class="text-[12px] text-[#2D1810]">{{$t('guide.title20')}}(USD)</div>
+            <div class="text-[#FF7D01] text-[9px] py-[3px] font-bold"><span class="pr-[10px] ">{{ item.num1 }}%</span>{{$t('guide.title21')}}</div>
+          </div>
+        </div>
+      </div>
+      
+
+    </div>
+    <div class=" text-[#000000]">
+      <div class="text-[14px] mt-[18px]">{{$t('guide.title23')}}</div>
+      <div class="text-[12px] text-[#000] py-[10px]">{{$t('guide.content7')}}</div>
+    </div>
+    
+    
+    
+
+    </div>
+     
   </div>
 </template>
 <script setup>
 import { computed, onMounted, onUnmounted, reactive, ref } from "vue";
 import { useCommonStore } from '@/store/modules/common';
 import {getConfigByLang} from "../../api/apis"
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+
 const incomeGuideEn = ref('')
 const commonStore = useCommonStore();
 
@@ -27,6 +96,81 @@ const parLang = computed(() => {
   return mapped ?? commonStore.lang; // 映射不到就用原值兜底
 });
 
+const guideArr =[
+  {
+    title: `${t('guide.title10')}`,
+    num:100,
+    num1:10
+  },
+  {
+    title: `${t('guide.title11')}`,
+    num:500,
+    num1:60
+  },
+  {
+    title: `${t('guide.title12')}`,
+    num:1000,
+    num1:120
+  },
+  {
+    title: `${t('guide.title13')}`,
+    num:1600,
+    num1:200
+  },
+  {
+    title: `${t('guide.title14')}`,
+    num:5500,
+    num1:1200
+  },
+  {
+    title: `${t('guide.title15')}`,
+    num:10000,
+    num1:2400
+  },
+]
+
+const advanArr = [
+  {
+    num:"1,500-9,999",
+    num1:4
+  },
+  {
+    num:"10,000-19,999",
+    num1:8
+  },
+  {
+    num:"20,000-49,999",
+    num1:4
+  },
+  {
+    num:`50,000- ${t('guide.title22')}`,
+    num1:4
+  }
+
+]
+
+const guide = [
+  {
+    day:2,
+    money:130
+  },
+  {
+    day:4,
+    money:'1,200'
+  },
+  {
+    day:7,
+    money:'1,500'
+  },
+  {
+    day:15,
+    money:'1,800'
+  },
+  {
+    day:30,
+    money:'2,370'
+  },
+]
 
 
 const getGetGlobalConfig = async() =>{
@@ -41,3 +185,10 @@ onMounted(() =>{
 })
 const onClickLeft = () => history.back();
 </script>
+<style scoped>
+  .guideBg {
+    width: 100%;
+    background-image: url(@/static/images/guideBg.png);
+    background-size: 100% 100%;
+  }
+</style>
