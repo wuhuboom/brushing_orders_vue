3a9913b9edd900e69650470ec7269c54c54425b9
hellowoldcrk <ttycracker@gmail.com>
2026-07-19 23:18:09 +0800

    修改config文件

--- public/config.js
+++ public/config.js
@@ -1,5 +1,7 @@
 window.g = {
-  VITE_API_BASE_URL: "https://api.ainimeme.bond/api",
-  VITE_API_IMG_URL: "https://api.ainimeme.bond",
+  // VITE_API_BASE_URL: "https://api.ainimeme.bond/api",
+  // VITE_API_IMG_URL: "https://api.ainimeme.bond",
+  VITE_API_BASE_URL: "https://api-shuadan.pigk.xyz/api",
+  VITE_API_IMG_URL: "https://api-shuadan.pigk.xyz",
   VITE_TITLE: "WEBW",
 };
